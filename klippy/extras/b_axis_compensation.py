3aeae92664ad5ede4dc3d7d3fe7a69f402f0e420
Ilya Gushchin <frylock3434@gmail.com>
2021-11-09 16:02:47 +0300

    Update b_axis_compensation.py

--- klippy/extras/b_axis_compensation.py
+++ klippy/extras/b_axis_compensation.py
@@ -33,8 +33,8 @@ class BAxisCompensation:
             'SAVE_B_AXIS_POINT', self.cmd_SAVE_B_AXIS_POINT,
             desc=self.cmd_SAVE_B_AXIS_POINT_help)
         self.gcode.register_command(
-            'B_AXIS_COMPENSATION_PROFILE', self.cmd_B_AXIS_COMPENSATION_PROFILE,
-            desc=self.cmd_B_AXIS_COMPENSATION_PROFILE_help
+            'B_AXIS_COMPENSATION_VARS', self.cmd_B_AXIS_COMPENSATION_VARS,
+            desc=self.cmd_B_AXIS_COMPENSATION_VARS_help
         )
         self.printer.register_event_handler("klippy:connect",
                                             self._handle_connect)
@@ -66,7 +66,19 @@ class BAxisCompensation:
         return newpos
 
     def calc_untransformed(self, pos):
-        return pos
+        a = pos[3]
+        sin_a = math.sin(a)
+        cos_a = math.cos(a)
+        a_rot_matrix = [1., 0., 0, 0., cos_a, -sin_a, 0., sin_a, cos_a]
+        pos[0] -= self.rot_center_x
+        pos[2] -= self.rot_center_z
+        m = matrix3x3_mul(matrix3x3_mul(
+            a_rot_matrix, self.rot_matrix), matrix3x3_transpose(a_rot_matrix))
+        m = matrix3x3_transpose(m)
+        newpos = matrix3x3_apply(pos, m)
+        newpos[0] += self.rot_center_x
+        newpos[2] += self.rot_center_z
+        return newpos
 
     def cmd_SAVE_B_AXIS_POINT(self, gcmd):
         point_idx = gcmd.get_int('POINT', 0)
@@ -95,28 +107,48 @@ class BAxisCompensation:
             self.calibration_template_size * sin_b
         rot_center_z = point_0[2] - \
             self.calibration_template_size * cos_b
-        configfile = self.printer.lookup_object('configfile')
-        configfile.set('b_axis_compensation', 'b_angle', self.b_angle)
-        configfile.set('b_axis_compensation',
-                       'rot_center_x', self.rot_center_x)
-        configfile.set('b_axis_compensation',
-                       'rot_center_z', self.rot_center_z)
+        return b_angle, rot_center_x, rot_center_z
 
     def _update_compensation(self, b_angle, rot_center_x, rot_center_z):
         self.b_angle = b_angle
         self.rot_center_x = rot_center_x
         self.rot_center_z = rot_center_z
+        sin_b = math.sin(b_angle)
+        cos_b = math.cos(b_angle)
+        self.rot_matrix = [cos_b, 0., sin_b, 0., 1., 0., -sin_b, 0., cos_b]
         gcode_move = self.printer.lookup_object('gcode_move')
         gcode_move.reset_last_position()
 
+    def _save_compensation(self, b_angle, rot_center_x, rot_center_z):
+        configfile = self.printer.lookup_object('configfile')
+        configfile.set('b_axis_compensation', 'b_angle', b_angle)
+        configfile.set('b_axis_compensation',
+                       'rot_center_x', rot_center_x)
+        configfile.set('b_axis_compensation',
+                       'rot_center_z', rot_center_z)
+
     def cmd_CALC_B_AXIS_COMPENSATION(self, gcmd):
-        self._calc_b_axis_compensation(
+        b_angle, rot_center_x, rot_center_z = self._calc_b_axis_compensation(
             self.point_coords[0], self.point_coords[1])
         enable = gcmd.get_int('ENABLE', 0)
         if enable:
-            # TODO: enable comp
+            self._update_compensation(b_angle, rot_center_x, rot_center_z)
+        save = gcmd.get_int('SAVE', 0)
+        if save:
+            self._save_compensation(b_angle, rot_center_x, rot_center_z)
 
     cmd_CALC_B_AXIS_COMPENSATION_help = "Calculate B axis compensation"
+
+    def cmd_B_AXIS_COMPENSATION_VARS(self, gcmd):
+        b_angle = gcmd.get_float('B', self.b_angle)
+        rot_center_x = gcmd.get_float('X', self.rot_center_x)
+        rot_center_z = gcmd.get_float('Z', self.rot_center_z)
+        self._update_compensation(b_angle, rot_center_x, rot_center_z)
+        save = gcmd.get_int('SAVE', 0)
+        if save:
+            self._save_compensation(b_angle, rot_center_x, rot_center_z)
+
+    cmd_B_AXIS_COMPENSATION_VARS_help = "Set B axis compensation parameters directly"
 
 
 def load_config(config):
