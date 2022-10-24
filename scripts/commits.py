ae3ab7ae1e86dca606c17bd915d93c16467522ca
Alexandr_Shchuk <shchukinaa2002@gmail.com>
2022-10-24 10:13:47 +0300

    STEAPP-185 test

---
+++ scripts/commits.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-c', '--commits',
+    metavar='commits',
+    help="commits from github")
+cmd_line_args = parser.parse_args()
+commits = cmd_line_args.commits
+print(commits)
