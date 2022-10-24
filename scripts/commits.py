357229095457ed348c123fdcc5381943d52dc653
Alexandr_Shchuk <shchukinaa2002@gmail.com>
2022-10-24 12:24:43 +0300

    STEAPP-185 пробую записывать данные в json

--- scripts/commits.py
+++ scripts/commits.py
@@ -15,7 +15,7 @@ cmd_line_args = parser.parse_args()
 commits = cmd_line_args.commits
 with open(commits, "r") as f:
     commits = f.read()
-json_commits = json.dumps(commits)
+json_commits = json.loads(commits)
 message_list = []
 for commit in json_commits:
     print(commit)
