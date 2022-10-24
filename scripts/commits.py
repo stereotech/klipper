55a5dbc1a5575d7a64ac7fab3038f93f29282fbd
Alexandr_Shchuk <shchukinaa2002@gmail.com>
2022-10-24 14:24:49 +0300

    STEAPP-185 рабочее получение списка комитов

--- scripts/commits.py
+++ scripts/commits.py
@@ -1,26 +1,28 @@
 import argparse
 import json
-from typing import (
-    Dict,
-    List,
-    Any,
-)
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '-c', '--commits',
-    metavar='commits',
-    help="commits from github")
-cmd_line_args = parser.parse_args()
-commits_link = cmd_line_args.commits
-with open(commits_link, "r", encoding='utf-8') as f:
-    commits = json.load(f)
-message_list = []
-for commit in commits:
-    message = commit['message']
-    message_list.append(message)
-for message in message_list:
-    print(message)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c', '--commits',
+        metavar='commits',
+        help="commits from github")
+    cmd_line_args = parser.parse_args()
+    commits_link = cmd_line_args.commits
+    with open(commits_link, "r", encoding='utf-8') as f:
+        commits = json.load(f)
+    message_list = []
+    for commit in commits:
+        message = commit['message']
+        message_list.append(message)
+    for message in message_list:
+        print(message)
+        
+    return message_list
+
+if __name__ == '__main__':
+    main()
 # 
 # for commit in commits:
 #     print(commit)
