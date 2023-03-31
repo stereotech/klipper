c92b5006ece346349fec22b9f11110a55e8b5fb1
sokolovjek <sokolovgek@mail.ru>
2023-03-31 16:43:09 +0000

    STEAPP-344: changed the calculation of skew compensation.

--- klippy/extras/skew_correction.py
+++ klippy/extras/skew_correction.py
@@ -37,6 +37,7 @@ def constrain(val, min_val, max_val):
 class PrinterSkew:
     def __init__(self, config):
         self.printer = config.get_printer()
+        self.gcode_move = self.printer.lookup_object('gcode_move')
         self.name = config.get_name()
         self.enabled = False
         self.xy_factor = 0.
@@ -52,6 +53,10 @@ class PrinterSkew:
         self._load_storage(config)
         self.printer.register_event_handler("klippy:connect",
                                             self._handle_connect)
+        self.printer.register_event_handler("gcode_move:change_wcs_lists",
+                                            self._change_wcs_lists)
+        # self.printer.register_event_handler("gcode_move:change_current_wcs",
+        #                                     self._change_current_wcs)
         self.next_transform = None
         gcode = self.printer.lookup_object('gcode')
         gcode.register_command('GET_CURRENT_SKEW', self.cmd_GET_CURRENT_SKEW,
@@ -70,24 +75,34 @@ class PrinterSkew:
                                 self.cmd_CALC_SKEW_COMPENSATION,
                                 desc=self.cmd_CALC_SKEW_COMPENSATION_help)
         self.point_coords = [
-            [0., 0., 0., 0., 0., 0.],
-            [0., 0., 0., 0., 0., 0.],
-            [0., 0., 0., 0., 0., 0.],
-            [0., 0., 0., 0., 0., 0.]
+            [0., 0., 0.],
+            [0., 0., 0.],
+            [0., 0., 0.],
+            [0., 0., 0.]
         ]
-        self.gcode_move = self.printer.lookup_object('gcode_move')
-        self.wcs_list = []
-        self.current_wcs = [0., 0., 0.]
+        self.wcs_list = [
+            [0., 0., 0.],
+            [0., 0., 0.],
+            [0., 0., 0.],
+            [0., 0., 0.],
+            [0., 0., 0.]
+        ]
         self.next_transform = None
+        self.start_z_point = 0.
 
     def _handle_connect(self):
         kin = self.printer.lookup_object('toolhead').get_kinematics()
         self.axes_min = kin.axes_min
         self.axes_max = kin.axes_max
-        self.wcs_list = self.printer.lookup_object('gcode_move').wcs_offsets
+        self.wcs_list = list(self.gcode_move.wcs_offsets)
         # Register transform
-        gcode_move = self.printer.lookup_object('gcode_move')
-        self.next_transform = gcode_move.set_move_transform(self, force=True)
+        self.next_transform = self.gcode_move.set_move_transform(self, force=True)
+
+    def _change_wcs_lists(self):
+        self.wcs_list = list(self.gcode_move.wcs_offsets)
+
+    # def _change_current_wcs(self):
+    #     self.current_wcs = self.gcode_move.current_wcs
 
     def cmd_SAVE_SKEW_POINT(self, gcmd):
         point_idx = gcmd.get_int('POINT', 0)
@@ -126,38 +141,38 @@ class PrinterSkew:
         if factor_name in factors:
             if factor_name == factors[0]:
                 # xy_factor
-                self.c_point = get_point(self.point_coords[0], self.point_coords[1])
-                self.b_point = list(self.c_point)
-                self.b_point[0] = self.c_point[0] - 50.
-                self.d_point = get_point(self.point_coords[2], self.point_coords[3])
-                self.a_point = list(self.d_point)
-                self.a_point[0] = self.d_point[0] - 50.
-                bc = length_side(self.b_point[0], self.b_point[1], self.c_point[0], self.c_point[1])
-                bd = length_side(self.b_point[0], self.b_point[1], self.d_point[0], self.d_point[1])
-                ac = length_side(self.a_point[0], self.a_point[1], self.c_point[0], self.c_point[1])
+                a_point = get_point(self.point_coords[2], self.point_coords[3])
+                d_point = list(a_point)
+                d_point[0] = d_point[0] + 50.
+                b_point = get_point(self.point_coords[0], self.point_coords[1])
+                c_point = list(b_point)
+                c_point[0] = c_point[0] + 50.
+                bc = length_side(b_point[0], b_point[1], c_point[0], c_point[1])
+                bd = length_side(b_point[0], b_point[1], d_point[0], d_point[1])
+                ac = length_side(a_point[0], a_point[1], c_point[0], c_point[1])
             elif factor_name == factors[1]:
                 # xz_factor
-                self.c_point = get_point(self.point_coords[0], self.point_coords[1])
-                self.b_point = list(self.c_point)
-                self.b_point[0] = self.c_point[0] - 50
-                self.d_point = get_point(self.point_coords[2], self.point_coords[3])
-                self.a_point = list(self.d_point)
-                self.a_point[0] = self.d_point[0] - 50
-                bc = length_side(self.b_point[0], self.b_point[2], self.c_point[0], self.c_point[2])
-                bd = length_side(self.b_point[0], self.b_point[2], self.d_point[0], self.d_point[2])
-                ac = length_side(self.a_point[0], self.a_point[2], self.c_point[0], self.c_point[2])
+                a_point = get_point(self.point_coords[2], self.point_coords[3])
+                d_point = list(a_point)
+                d_point[0] = d_point[0] + 50
+                b_point = get_point(self.point_coords[0], self.point_coords[1])
+                c_point = list(b_point)
+                c_point[0] = c_point[0] + 50
+                bc = length_side(b_point[0], b_point[2], c_point[0], c_point[2])
+                bd = length_side(b_point[0], b_point[2], d_point[0], d_point[2])
+                ac = length_side(a_point[0], a_point[2], c_point[0], c_point[2])
             else:
                 # yz_factor
-                b_point = self.point_coords[0]
-                a_point = get_point(self.point_coords[1], self.point_coords[2])
+                a_point = self.point_coords[0]
                 d_point = list(a_point)
                 d_point[1] = d_point[1] - 50
+                b_point = get_point(self.point_coords[1], self.point_coords[2])
                 c_point = list(b_point)
                 c_point[1] = c_point[1] - 50
                 bc = length_side(b_point[1], b_point[2], c_point[1], c_point[2])
                 bd = length_side(b_point[1], b_point[2], d_point[1], d_point[2])
                 ac = length_side(a_point[1], a_point[2], c_point[1], c_point[2])
-            factor_value = float("%.4f" % calc_skew_factor(ac, bd, bc)) / 2
+            factor_value = float("%.4f" % calc_skew_factor(ac, bd, bc))
             factor_name = factor_name.lower() + '_factor'
             setattr(self, factor_name, factor_value)
             out = "Calculated skew compensation %s: %.6f radians, %.2f degrees\n" % (
@@ -183,18 +198,18 @@ class PrinterSkew:
 
     def calc_skew(self, pos):
         newpos = list(pos)
-        newpos[0] = pos[0] - (pos[1]- self.current_wcs[1]) * self.xy_factor \
-            - (pos[2] - self.current_wcs[2]) * (self.xz_factor - (self.xy_factor * self.yz_factor))
-        newpos[1] = pos[1] - (pos[2]- self.current_wcs[2]) * self.yz_factor
+        pos_z = pos[2] - self.start_z_point
+        newpos[0] = pos[0] - pos_z * self.xz_factor
+        newpos[1] = pos[1] - pos_z * ((self.yz_factor * -1) / 2.)
         newpos = [constrain(newpos[axis], self.axes_min[axis], self.axes_max[axis]) for axis in range(5)]
         newpos.append(pos[5])
         return newpos
 
     def calc_unskew(self, pos):
         newpos = list(pos)
-        newpos[0] = pos[0] + (pos[1] - self.current_wcs[1]) * self.xy_factor \
-            + (pos[2] - self.current_wcs[2]) * self.xz_factor
-        newpos[1] = pos[1] + (pos[2]- self.current_wcs[2]) * self.yz_factor
+        pos_z = pos[2] - self.start_z_point
+        newpos[0] = pos[0] + pos_z * self.xz_factor
+        newpos[1] = pos[1] + pos_z * ((self.yz_factor * -1) / 2.)
         newpos = [constrain(newpos[axis], self.axes_min[axis], self.axes_max[axis]) for axis in range(5)]
         newpos.append(pos[5])
         return newpos
@@ -205,7 +220,8 @@ class PrinterSkew:
         return self.calc_unskew(self.next_transform.get_position())
 
     def move(self, newpos, speed):
-        axes_d = [self.next_transform.get_position()[i] - newpos[i] for i in
+        old_pos = self.next_transform.get_position()
+        axes_d = [old_pos[i] - newpos[i] for i in
                                 (0, 1, 2, 3, 4, 5)]
         move_d = math.sqrt(sum([d * d for d in axes_d[:5]]))
         if not self.enabled or move_d < .000000001:
@@ -242,7 +258,6 @@ class PrinterSkew:
             gcmd.respond_info('Skew compensation enabled.')
         else:
             self.enabled = False
-            self.current_wcs = self.wcs_list[0]
             gcmd.respond_info('Skew compensation disabled.')
         planes = ["XY", "XZ", "YZ"]
         for plane in planes:
@@ -258,7 +273,8 @@ class PrinterSkew:
                     raise gcmd.error(
                         "skew_correction: improperly formatted entry for "
                         "plane [%s]\n%s" % (plane, gcmd.get_commandline()))
-    cmd_SET_SKEW_help = "Set skew based on lengths of measured object"
+    cmd_SET_SKEW_help = "Set skews(XY=0.1, ...), delete skews(CLEAR=1) \
+        or include skew correction(ENABLE=1) its."
 
     def cmd_SKEW_PROFILE(self, gcmd):
         options = collections.OrderedDict({
@@ -286,11 +302,9 @@ class PrinterSkew:
         """
         self.current_profile = prof_name
         if prof_name == 'module_3d':
-            self.current_wcs = self.wcs_list[0]
-        # getting min value on wcs_list
+            self.start_z_point = self.wcs_list[0]
         else:
-            self.current_wcs = list(self.wcs_list[3])
-            self.current_wcs[2] = self.wcs_list[4][2]
+           self.start_z_point = (self.wcs_list[3][2] + self.wcs_list[4][2]) / 2.
         profile = self.skew_profiles.get(prof_name)
         if profile is not None:
             self._update_skew(profile['xy_skew'], profile['xz_skew'], profile['yz_skew'])
@@ -341,8 +355,8 @@ class PrinterSkew:
             'current_yz_factor': self.yz_factor,
             'skew_profiles': self.skew_profiles,
             'wcs_list': self.wcs_list,
-            'current_wcs': self.current_wcs,
-            'current_profile': self.current_profile
+            'current_profile': self.current_profile,
+            'start_z_point': self.start_z_point
         }
 
 def load_config(config):
