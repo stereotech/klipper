a30d3445dfd459f43cb3a7cb8f8b70589f10ae8e
sokolovjek <sokolovgek@mail.ru>
2023-06-27 07:14:30 +0100

    STEAPP-547: refactoring the code.

--- klippy/extras/auto_wcs.py
+++ klippy/extras/auto_wcs.py
@@ -48,14 +48,8 @@ class AutoWcs:
             'SET_PROBE_BACKLASH', self.cmd_SET_PROBE_BACKLASH,
             desc=self.cmd_SET_PROBE_BACKLASH_help)
         self.gcode.register_command(
-            'CALC_WCS_ONE_X', self.cmd_CALC_WCS_ONE_X,
-            desc=self.cmd_CALC_WCS_ONE_X_help)
-        self.gcode.register_command(
-            'CALC_WCS_ONE_Y', self.cmd_CALC_WCS_ONE_Y,
-            desc=self.cmd_CALC_WCS_ONE_Y_help)
-        self.gcode.register_command(
-            'CALC_WCS_TWO_X', self.cmd_CALC_WCS_TWO_X,
-            desc=self.cmd_CALC_WCS_TWO_X_help)
+            'CALC_WCS', self.cmd_CALC_WCS,
+            desc=self.cmd_CALC_WCS_help)
 
     def _calc_wcs_old_sensor(self, thickness, adj, gcmd):
         thickness = thickness / 2.0
@@ -109,35 +103,17 @@ class AutoWcs:
         self.probe_backlash_y = abs(self.point_coords[5][1] - y2)
         self.probe_backlash_y_2 = abs(self.point_coords[1][1] - (y1 - 5))
 
-    cmd_CALC_WCS_ONE_X_help = "command for calculate wcs_1_x coordinate for SPIRALL-FULL."
-    def cmd_CALC_WCS_ONE_X(self, gcmd):
-        x = (self.point_coords[0][0] + self.point_coords[1][0]) / 2.
+    cmd_CALC_WCS_help = "command for calculate wcs coordinate for SPIRALL-FULL."
+    def cmd_CALC_WCS(self, gcmd):
+        wcs =  gcmd.get_int('WCS')
+        ind_axis = gcmd.get_int('AXIS')
+        new_axis = (self.point_coords[0][ind_axis] + self.point_coords[1][ind_axis]) / 2.
         gcode_move = self.printer.lookup_object('gcode_move')
-        x_raw = gcode_move.wcs_offsets[1][0]
-        diff_x = x - x_raw
-        gcmd.respond_info("""calculated wcs_1_x=%f,
-            difference between tool and template=%f.""" % (x, diff_x))
-        return x
-
-    cmd_CALC_WCS_TWO_X_help = "command for calculate wcs_2_x coordinate for SPIRALL-FULL."
-    def cmd_CALC_WCS_TWO_X(self, gcmd):
-        x = (self.point_coords[0][0] + self.point_coords[1][0]) / 2.
-        gcode_move = self.printer.lookup_object('gcode_move')
-        x_raw = gcode_move.wcs_offsets[2][0]
-        diff_x = x - x_raw
-        gcmd.respond_info("""calculated wcs_2_x=%f,
-            difference between tool and template=%f.""" % (x, diff_x))
-        return x
-
-    cmd_CALC_WCS_ONE_Y_help = "command for calculate wcs_1_y coordinate for SPIRALL-FULL."
-    def cmd_CALC_WCS_ONE_Y(self, gcmd):
-        y = (self.point_coords[0][1] + self.point_coords[1][1]) / 2.
-        gcode_move = self.printer.lookup_object('gcode_move')
-        y_raw = gcode_move.wcs_offsets[1][1]
-        diff_y = y - y_raw
-        gcmd.respond_info("""calculated wcs_1_y=%f,
-            difference between tool and template=%f.""" % (y, diff_y))
-        return y
+        old_axis = gcode_move.wcs_offsets[wcs][ind_axis]
+        diff_axis = new_axis - old_axis
+        gcmd.respond_info("""calculated wcs_%d_%d=%f,
+            difference between tool and template=%f.""" % (wcs, ind_axis, new_axis, diff_axis))
+        return new_axis
 
     def cmd_GET_RADIUS_TOOLING(self, gcmd):
         x1, y1 = self.point_coords[1][0] + self.probe_backlash_y, self.point_coords[1][1]
