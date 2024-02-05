4c97fdd57fe0de98b3592c3aef5eceddc7cc9bd5
sokolovjek <sokolovgek@mail.ru>
2024-02-05 14:46:04 +0000

    STEAPP-733: Added recursive offset measurement for the С-axis

--- klippy/extras/c_axis_align.py
+++ klippy/extras/c_axis_align.py
@@ -10,13 +10,15 @@ class CAxisAlignCalculation:
         self.printer = config.get_printer()
         self.gcode_move = self.printer.load_object(config, 'gcode_move')
         self.gcode = self.printer.lookup_object('gcode')
+        self.max_repeat_probe = config.getint('max_repeat_probe', 5)
+        self.threshold_value = config.getfloat('threshold_value', 0.02)
         self.point_coords = [
             [0., 0., 0., 0., 0., 0.],
             [0., 0., 0., 0., 0., 0.]
         ]
-        self.gcode.register_command('CALC_C_AXIS_ALIGN',
-                                    self.cmd_CALC_C_AXIS_ALIGN,
-                                    desc=self.cmd_CALC_C_AXIS_ALIGN_help)
+        self.gcode.register_command('ALIGN_C_AXIS',
+                                    self.cmd_ALIGN_C_AXIS,
+                                    desc=self.cmd_ALIGN_C_AXIS_help)
         self.gcode.register_command(
             'SAVE_C_AXIS_POINT', self.cmd_SAVE_C_AXIS_POINT,
             desc=self.cmd_SAVE_C_AXIS_POINT_help)
@@ -41,18 +43,28 @@ class CAxisAlignCalculation:
 
     def _calc_c_axis_align(self, point_0, point_1):
         offset = math.atan((point_1[1] - point_0[1]) / 90) * RAD_TO_DEG / 3
+        logging.info("calculate offset for the axis C: %f." % offset)
         return offset
 
-    def cmd_CALC_C_AXIS_ALIGN(self, gcmd):
-        offset = self._calc_c_axis_align(
-            self.point_coords[0], self.point_coords[1])
+    def _apply_offset_c(self, offset):
         align_gcmd = self.gcode.create_gcode_command(
             'G0', 'G0', {'C': offset})
         self.gcode_move.cmd_G1(align_gcmd)
         self.gcode_move.cmd_G92(self.gcode.create_gcode_command(
             'G92', 'G92', {'C': 0}))
 
-    cmd_CALC_C_AXIS_ALIGN_help = "Calculate C axis align"
+    def cmd_ALIGN_C_AXIS(self, gcmd):
+        for i in range(self.max_repeat_probe):
+            self.gcode.run_script_from_command("MOVE_ALIGN_C_AXIS")
+            offset = self._calc_c_axis_align(
+                self.point_coords[0], self.point_coords[1])
+            if (self.threshold_value * -1) <= offset <= self.threshold_value:
+                break
+            else:
+                self._apply_offset_a(offset)
+
+
+    cmd_ALIGN_C_AXIS_help = "Calculate C axis align"
 
 
 def load_config(config):
