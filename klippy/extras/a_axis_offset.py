3ae841288c83ee981ff34faec338e8d6056cd8e2
sokolovjek <sokolovgek@mail.ru>
2024-02-05 14:04:38 +0000

    STEAPP743: Added recursive offset measurement for the A-axis

--- klippy/extras/a_axis_offset.py
+++ klippy/extras/a_axis_offset.py
@@ -1,6 +1,8 @@
 # Helper script to automate a axis offset calculation
 
 import math
+import logging
+
 
 RAD_TO_DEG = 57.295779513
 
@@ -10,15 +12,18 @@ class AAxisOffsetCalculation:
         self.printer = config.get_printer()
         self.gcode_move = self.printer.load_object(config, 'gcode_move')
         self.gcode = self.printer.lookup_object('gcode')
+        self.calc_offset = 0.
+        self.max_repeat_probe = config.getint('max_repeat_probe', 5)
+        self.threshold_value = config.getfloat('threshold_value', 0.02)
         self.point_coords = [
             [0., 0., 0., 0., 0., 0.],
             [0., 0., 0., 0., 0., 0.],
             [0., 0., 0., 0., 0., 0.],
             [0., 0., 0., 0., 0., 0.]
         ]
-        self.gcode.register_command('CALC_A_AXIS_OFFSET',
-                                    self.cmd_CALC_A_AXIS_OFFSET,
-                                    desc=self.cmd_CALC_A_AXIS_OFFSET_help)
+        self.gcode.register_command(
+            'ALIGN_A_AXIS', self.cmd_ALIGN_A_AXIS,
+            desc=self.cmd_ALIGN_A_AXIS_help)
         self.gcode.register_command(
             'SAVE_A_AXIS_POINT', self.cmd_SAVE_A_AXIS_POINT,
             desc=self.cmd_SAVE_A_AXIS_POINT_help)
@@ -41,37 +46,46 @@ class AAxisOffsetCalculation:
                 self.point_coords[point_idx][axis] = coord
 
     def _calc_a_axis_offset(self, point_0, point_1):
-        offset = RAD_TO_DEG * math.asin((point_1[2] - point_0[2]) / math.hypot(
-            point_1[1] - point_0[1], point_1[2] - point_0[2]))
+        """
+                  . b
+               .  .
+             .    .
+         a  . . . . c
+        """
+        bc = point_1[2] - point_0[2]
+        ac = point_1[1] - point_0[1]
+        ab =  math.hypot(ac, bc)
+        asin_a =  math.asin(bc / ab)
+        offset = RAD_TO_DEG * asin_a
+        logging.info("calculate offset A=%f." % offset)
         return offset
 
-    cmd_CALC_A_AXIS_OFFSET_help = "Calculate A axis offset"
-    def cmd_CALC_A_AXIS_OFFSET(self, gcmd):
-        offset = self._calc_a_axis_offset(
-            self.point_coords[0], self.point_coords[1])
+    def _apply_offset_a(self):
         homing_origin_a = self.gcode_move.get_status()['homing_origin'].a
-        if homing_origin_a + offset > 0.0:
-            offset = 0.0
+        if (homing_origin_a + self.calc_offset) > 0.0:
+            logging.warning("A-axis offset is positive")
+            self.calc_offset = 0.0
         offset_gcmd = self.gcode.create_gcode_command(
-            'SET_GCODE_OFFSET', 'SET_GCODE_OFFSET', {'A_ADJUST': offset})
+            'SET_GCODE_OFFSET', 'SET_GCODE_OFFSET', {'A_ADJUST': self.calc_offset})
         self.gcode_move.cmd_SET_GCODE_OFFSET(offset_gcmd)
-        gcmd.respond_info("calculate offset for the axis A: %f." % offset)
+        logging.info("----------------apply offset for the axis A: %f." % self.calc_offset)
 
-    # cmd_CALC_A_AXIS_OFFSET_TOOL_help = "Calculate A axis offset by tool"
-    # def cmd_CALC_A_AXIS_OFFSET_TOOL(self, gcmd):
-    #     # get average value axis Z
-    #     self.point_coords[0][2] = (self.point_coords[0][2] + self.point_coords[2][2]) / 2.
-    #     self.point_coords[1][2] = (self.point_coords[1][2] + self.point_coords[3][2]) / 2.
-    #     # calculate offset
-    #     offset = self._calc_a_axis_offset(
-    #         self.point_coords[0], self.point_coords[1])
-    #     homing_origin_a = self.gcode_move.get_status()['homing_origin'].a
-    #     if homing_origin_a + offset > 0.0:
-    #         offset = 0.0
-    #     offset_gcmd = self.gcode.create_gcode_command(
-    #         'SET_GCODE_OFFSET', 'SET_GCODE_OFFSET', {'A_ADJUST': offset})
-    #     self.gcode_move.cmd_SET_GCODE_OFFSET(offset_gcmd)
-    #     gcmd.respond_info("calculate offset for the axis A: %f." % offset)
+    cmd_ALIGN_A_AXIS_help = "Calculate A axis offset"
+    def cmd_ALIGN_A_AXIS(self, gcmd):
+        mode = gcmd.get('MODE')
+        for i in range(self.max_repeat_probe):
+            self.gcode.run_script_from_command("MOVE_ALIGN_A_AXIS MODE=%s" % mode)
+            self.calc_offset = self._calc_a_axis_offset(self.point_coords[0], self.point_coords[1])
+            if (self.threshold_value * -1) <= self.calc_offset <= self.threshold_value:
+                break
+            else:
+                self._apply_offset_a()
+
+    def get_status(self, eventtime=None):
+        return {
+            "point_coords": self.point_coords,
+            "calc_offset": self.calc_offset
+        }
 
 
 def load_config(config):
