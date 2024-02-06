1e5a0d920cc940ae308e82f277f4aab5d5ed5d15
sokolovjek <sokolovgek@mail.ru>
2024-02-06 07:37:07 +0000

    STEAPP-733: Added recursive offset measurement for the С-axis

--- klippy/extras/c_axis_align.py
+++ klippy/extras/c_axis_align.py
@@ -1,6 +1,8 @@
 # Helper script to automate a axis offset calculation
 
 import math
+import logging
+
 
 RAD_TO_DEG = 57.295779513
 
@@ -16,12 +18,14 @@ class CAxisAlignCalculation:
             [0., 0., 0., 0., 0., 0.],
             [0., 0., 0., 0., 0., 0.]
         ]
-        self.gcode.register_command('ALIGN_C_AXIS',
-                                    self.cmd_ALIGN_C_AXIS,
-                                    desc=self.cmd_ALIGN_C_AXIS_help)
+        self.gcode.register_command(
+            'ALIGN_C_AXIS', self.cmd_ALIGN_C_AXIS,
+            desc=self.cmd_ALIGN_C_AXIS_help)
         self.gcode.register_command(
             'SAVE_C_AXIS_POINT', self.cmd_SAVE_C_AXIS_POINT,
             desc=self.cmd_SAVE_C_AXIS_POINT_help)
+
+    cmd_SAVE_C_AXIS_POINT_help = "Save point for C axis align"
 
     def cmd_SAVE_C_AXIS_POINT(self, gcmd):
         point_idx = gcmd.get_int('POINT', 0)
@@ -39,19 +43,7 @@ class CAxisAlignCalculation:
             for axis, coord in enumerate(coords):
                 self.point_coords[point_idx][axis] = coord
 
-    cmd_SAVE_C_AXIS_POINT_help = "Save point for C axis align"
-
-    def _calc_c_axis_align(self, point_0, point_1):
-        offset = math.atan((point_1[1] - point_0[1]) / 90) * RAD_TO_DEG / 3
-        logging.info("calculate offset for the axis C: %f." % offset)
-        return offset
-
-    def _apply_offset_c(self, offset):
-        align_gcmd = self.gcode.create_gcode_command(
-            'G0', 'G0', {'C': offset})
-        self.gcode_move.cmd_G1(align_gcmd)
-        self.gcode_move.cmd_G92(self.gcode.create_gcode_command(
-            'G92', 'G92', {'C': 0}))
+    cmd_ALIGN_C_AXIS_help = "Calculate and apply correction for align the C axis"
 
     def cmd_ALIGN_C_AXIS(self, gcmd):
         for i in range(self.max_repeat_probe):
@@ -59,12 +51,23 @@ class CAxisAlignCalculation:
             offset = self._calc_c_axis_align(
                 self.point_coords[0], self.point_coords[1])
             if (self.threshold_value * -1) <= offset <= self.threshold_value:
+                logging.info("align the axis C completed")
                 break
             else:
-                self._apply_offset_a(offset)
+                self._apply_offset_c(offset)
 
+    def _calc_c_axis_align(self, point_0, point_1):
+        offset = math.atan((point_1[1] - point_0[1]) / 90) * RAD_TO_DEG / 3
+        logging.info("calculate offset for the axis C: %f." % offset)
+        return offset
 
-    cmd_ALIGN_C_AXIS_help = "Calculate C axis align"
+    def _apply_offset_c(self, offset):
+        logging.info("an offset has been applied to correct the C axis")
+        align_gcmd = self.gcode.create_gcode_command(
+            'G0', 'G0', {'C': offset})
+        self.gcode_move.cmd_G1(align_gcmd)
+        self.gcode_move.cmd_G92(self.gcode.create_gcode_command(
+            'G92', 'G92', {'C': 0}))
 
 
 def load_config(config):
